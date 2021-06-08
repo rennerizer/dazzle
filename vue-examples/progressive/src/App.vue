a68fd5e050c0f073db421291dc30ded87189f7e1
Nathan Renner <rennerizer@gmail.com>
2021-06-08 15:08:10 -0600

    progressive web app creation

---
+++ vue-examples/progressive/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+    
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-examples/progressive/src/mixins/octokitMixin.js
@@ -0,0 +1,12 @@
+import { Octokit }  from "https://cdn.skypack.dev/@octokit/rest";
+
+export const octokitMixin = {
+  methods: {
+    createOctokitClient() {
+      return new Octokit({
+        auth: localStorage.getItem("github-token")
+      });
+    }
+  }
+
+}
